7838e6066c2da76a4cec3c508647a3f8a3a18afc
Will Badart <wbadart@nd.edu>
2016-09-20 20:10:23 -0400

    First implementation Also added test files to source control

--- dfa.py
+++ dfa.py
@@ -11,12 +11,113 @@
 '
 '''
 
-import fileinput
-
 class DFA:
-    def __init__(self):
-        for i, line in enumerate(fileinput.input()):
-            if i == 1:
-                continue
-            if i == 2:
-                self.E = line.split(', ');
+
+    #====================================================
+    # Construct DFA model based on rules listed in FNAME
+    #====================================================
+
+    def __init__(self, fname):
+
+        fs = open(fname)
+        self.d = dict();
+
+        for i, line in enumerate(fs.readlines()):
+
+            line = line.rstrip()
+
+            # line 1 is metadata about the machine
+            if i == 0:
+                self.name = line
+
+            # the next line is the alphabet, sigma
+            elif i == 1:
+                self.E = line.split(',')
+
+            # line 3 is the set of states
+            elif i == 2:
+                self.Q = line.split(',')
+
+            # line 4 holds the initial state
+            elif i == 3:
+                self.q0 = line
+
+            # line 5 lists the accepting states
+            elif i == 4:
+                self.F = line.split(',')
+
+            # the remaining lines hold the transition rules
+            else:
+                transition = line.split(',')
+                print 'Rule #{}: {}'.format(len(self.d) + 1, line.rstrip())
+                self.d[(transition[0], transition[1])] = transition[2]
+
+        self.current_state = self.q0
+        fs.close()
+
+
+    #==================================================
+    # Update self.current_state based on an input char
+    #==================================================
+
+    def transition(self, char):
+        if (self.current_state, char) not in self.d:
+            return False
+        self.current_state = self.d[(self.current_state, char)]
+        return True
+
+
+    #===============================================
+    # Run the simulation on one input string (line)
+    #===============================================
+
+    def run(self, line):
+
+        line = line.rstrip()
+        print 'Machine "{}" processing string "{}"'.format(self.name, line)
+        print ', '.join(self.E)
+
+        for i, char in enumerate(line):
+
+            # Sanity checks
+            if char not in self.E:
+                print 'Character "{}" not in alphabet.'.format(char)
+                break
+            if (self.current_state, char) not in self.d:
+                print 'No rule for ({}, {}).'.format(self.current_state, char)
+                break
+
+            # Follow transition rule
+            next_state = self.d[(self.current_state, char)]
+            print 'Step #{}: ({}, {}) -> {}'.format(i + i, self.current_state, char, next_state)
+            self.transition(char)
+
+        print 'Accepted' if self.current_state in self.F else 'Rejected'
+        print
+
+
+    #==============================================
+    # Run the simulation on a file of test strings
+    #==============================================
+
+    def test(self, fname):
+        fs = open(fname)
+        for line in fs.readlines():
+            self.run(line)
+        fs.close()
+
+
+    #================================================================
+    # Step through the simulation, inputting one character at a time
+    #================================================================
+
+    def step(self):
+        print 'Initial state: {}'.format(self.q0)
+        str_in = raw_input('> ')
+        while True:
+            self.transition(str_in.rstrip())
+            print 'State: {}, {}'.format(\
+                    self.current_state, 'accepted' if self.current_state in self.F else 'rejected')
+            str_in = raw_input('> ')
+            if str_in == 'Q':
+                break
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 '''
 ' main.py
 '
@@ -9,3 +11,71 @@
 '
 '''
 
+import sys
+import getopt
+from os import path
+from dfa import DFA
+
+
+#================
+# Default values
+#================
+
+INTERACTIVE = False
+MACHINE_DEF = 'M1.txt'
+TEST_STRS   = 'M1-Accept.txt'
+
+
+#===================
+# Utility functions
+#===================
+
+def usage():
+    print >>sys.stderr, '''usage: {} [ -m FNAME -s FNAME -i -h ]
+    -m FNAME    read in machine definition from FNAME
+    -s FNAME    read in test strings from FNAME
+    -i          enable interactive mode
+    -h          disaply this help text'''.format(sys.argv[0])
+    sys.exit(0)
+
+def error(msg):
+    print >>sts.stderr, str(msg)
+    sys.exit(1)
+
+
+#============================
+# Parse command-line options
+#============================
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'm:s:ih')
+except getopt.GetoptError as err:
+    error(err)
+
+for o, a in opts:
+    if o == '-h':
+        usage()
+    elif o == '-i':
+        INTERACTIVE = True
+    elif o == '-m':
+        if path.isfile(a):
+            MACHINE_DEF = a
+        else:
+            error('No such file, "{}"'.format(a))
+    elif o == 's':
+        if path.isfile(a):
+            TEST_STRS = a
+        else:
+            error('No such file, "{}"'.format(a))
+
+
+#================
+# Run simulation
+#================
+
+uut = DFA(MACHINE_DEF)
+
+if INTERACTIVE:
+    uut.step()
+else:
+    uut.test(TEST_STRS)
